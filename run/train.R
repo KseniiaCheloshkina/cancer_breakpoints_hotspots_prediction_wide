f17e88b3f508d1683f0e254359f29a32c0a38edb
Kseniia Cheloshkina <kscheloshkina@gmail.com>
2019-10-03 16:22:26 +0300

    Update train.R

--- run/train.R
+++ run/train.R
@@ -1098,7 +1098,67 @@ all_data <- get_higher_level_features(data=data, features_cols = features_cols, 
 
 
 
+get_train_test_split <- function(data, target_col, start_pos_col, chr_col, feature_cols, train_ratio, seed){
+  
+  set.seed(seed)
+  
+  chrs <- c(
+    "1","2","3","4","5","6","7","8","9","10",
+    "11","12","13","14","15","16","17","18","19","20","21","22","X"
+  )
+  data[chr_col] <- as.character(data[chr_col])
+  
+  # get position bins
+  all_pos_map <- data.frame()
+  
+  for (chr in chrs){
+    starts <- data[data[chr_col] == chr, start_pos_col]
+    starts <- starts[order(starts)]
+    bin_name <- cut(starts, breaks = 4, labels = c("first_bin", "second_bin", "third_bin", "last_bin"),
+                    include.lowest = TRUE, right = FALSE)
+    pos_map <- data.frame(start = starts, pos_bin = bin_name, chr = chr)
+    names(pos_map) <- c(start_pos_col, "pos_bin", chr_col)
+    all_pos_map <- rbind(all_pos_map, pos_map)
+  }
+  all_pos_map[chr_col] <- as.character(all_pos_map[chr_col])
+  data <- data %>%
+    inner_join(all_pos_map, by = c(!!as.name(chr_col), !!as.name(start_pos_col)))
+  
+  # create strata column by chromosome and quantile of position in chromosome
+  data['strata'] <- paste0(data[chr_col], "_", data['pos_bin'])
+  
+  # split data on examples with positive and negative target
+  rownames(data) <- NULL
+  pos_label_data <- data[data[target_col] == 1, ]
+  neg_label_data <- data[data[target_col] == 0, ]
 
+  # split in train_ratio proportion stratified by strata column
+  tr_neg <- createDataPartition(neg_label_data$strata, p = train_ratio, list = FALSE)
+  data_train <- neg_label_data[tr_neg,]
+  data_test  <- neg_label_data[-tr_neg,]
+
+  tr_pos <- createDataPartition(pos_label_data$strata, p = train_ratio, list = FALSE)
+  data_train <- rbind(data_train, pos_label_data[tr_pos, ])
+  data_test <- rbind(data_test, pos_label_data[te_pos, ])
+
+  x_train <- data_train[feature_cols]
+  x_test <- data_test[feature_cols]
+
+  y_train <- as.factor(data_train[, target_col])
+  levels(y_train) <- c("X0", "X1")
+  y_test <- as.factor(data_test[, target_col])
+  levels(y_test) <- c("X0", "X1")
+  
+  all_data <- list()
+  all_data[[1]] <- x_train
+  all_data[[2]] <- y_train
+  all_data[[3]] <- x_test
+  all_data[[4]] <- y_test
+  
+  names(all_data) <- c("x_train", "y_train", "x_test", "y_test")
+  
+  return(all_data)
+}
 
 
 
